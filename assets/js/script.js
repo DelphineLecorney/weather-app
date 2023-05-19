dd213d3d8d974839278a20f1ee8daf6ae81a24f3
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-19 17:19:18 +0200

    Add api Unsplash

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,35 @@
-import { apiWeatherKey } from "./modules/keys.js";
+import { apiUnsplashKey, apiWeatherKey } from "./modules/keys.js";
 
 const form = document.querySelector('.weather-form');
 const cityInput = document.querySelector('#city-input');
-const api_WeatherKey = apiWeatherKey;
 const apiWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiUnsplashUrl = 'https://api.unsplash.com';
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const city = cityInput.value;
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
 
-    const fetchWeather = async () => {
-        try{
-            const loader = document.getElementById('loader');
-            loader.classList.remove('hidden');
+  const fetchWeather = async () => {
+    try {
+      const loader = document.getElementById('loader');
+      loader.classList.remove('loader-hidden');
 
-            const response = await fetch(`${apiWeatherUrl}?q=${city}&appid=${api_WeatherKey}`);
-            const data = await response.json();
+      const response = await fetch(`${apiWeatherUrl}?q=${city}&appid=${apiWeatherKey}`);
+      const responsePhoto = await fetch(`${apiUnsplashUrl}/photos/random?query=${city}&client_id=${apiUnsplashKey}`);
 
-            console.log(data);
+      const weatherData = await response.json();
+      const photoData = await responsePhoto.json();
 
-            loader.classList.add('hidden');
-        }catch(error) {
-            console.log(error);
-        }finally {
-            const loader = document.getElementById('loader');
-            loader.classList.add('hidden');
-        }
+      const cityPhoto = document.getElementById('city-photo');
+      cityPhoto.src = photoData.urls.regular;
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      const loader = document.getElementById('loader');
+      loader.classList.add('loader-hidden');
     }
-    fetchWeather();
+  };
+
+  fetchWeather();
 });
